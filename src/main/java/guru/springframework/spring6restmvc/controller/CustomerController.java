922c7565306236715853e64194dbcc7e6843a53f
Michele COnversa <michele.conversa@gmail.com>
2023-10-07 16:08:35 +0200

    Added REST POST

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Customer customer){
+
+        Customer savedCustomer = customerService.saveNewCustomer(customer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedCustomer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID id){
+        return customerService.getCustomerById(id);
+    }
+
+}
