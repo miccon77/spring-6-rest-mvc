c3163247b7b3a048d6dc113c76e280558c2bc3c5
Michele COnversa <michele.conversa@gmail.com>
2023-10-08 10:01:06 +0200

    Added junit tests

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -2,7 +2,7 @@ package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,13 +16,16 @@ import java.util.UUID;
  * Created by jt, Spring Framework Guru.
  */
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/beer")
 public class BeerController {
+
+    public static final String BEER_PATH = "/api/v1/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
+
     private final BeerService beerService;
 
-    @PatchMapping("{beerId}")
+    @PatchMapping(BEER_PATH_ID)
     public ResponseEntity updateBeerPatchById(@PathVariable("beerId")UUID beerId, @RequestBody Beer beer){
 
         beerService.patchBeerById(beerId, beer);
@@ -30,7 +33,7 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("{beerId}")
+    @DeleteMapping(BEER_PATH_ID)
     public ResponseEntity deleteById(@PathVariable("beerId") UUID beerId){
 
         beerService.deleteById(beerId);
@@ -38,7 +41,7 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("{beerId}")
+    @PutMapping(BEER_PATH_ID)
     public ResponseEntity updateById(@PathVariable("beerId")UUID beerId, @RequestBody Beer beer){
 
         beerService.updateBeerById(beerId, beer);
@@ -46,24 +49,23 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping
-    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping(BEER_PATH)
     public ResponseEntity handlePost(@RequestBody Beer beer){
 
         Beer savedBeer = beerService.saveNewBeer(beer);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+        headers.add("Location", BEER_PATH + "/" + savedBeer.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(value = BEER_PATH)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    @GetMapping(value = BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by Id - in controller");
--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -1,99 +1,105 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Customer;
-import java.time.LocalDateTime;
-import java.util.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-/** Created by jt, Spring Framework Guru. */
+import java.time.LocalDateTime;
+import java.util.*;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-  private Map<UUID, Customer> customerMap;
+    private Map<UUID, Customer> customerMap;
 
-  public CustomerServiceImpl() {
-    Customer customer1 =
-        Customer.builder()
-            .id(UUID.randomUUID())
-            .name("Customer 1")
-            .version(1)
-            .createdDate(LocalDateTime.now())
-            .updateDate(LocalDateTime.now())
-            .build();
+    public CustomerServiceImpl() {
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Customer 1")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
 
-    Customer customer2 =
-        Customer.builder()
-            .id(UUID.randomUUID())
-            .name("Customer 2")
-            .version(1)
-            .createdDate(LocalDateTime.now())
-            .updateDate(LocalDateTime.now())
-            .build();
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Customer 2")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
 
-    Customer customer3 =
-        Customer.builder()
-            .id(UUID.randomUUID())
-            .name("Customer 3")
-            .version(1)
-            .createdDate(LocalDateTime.now())
-            .updateDate(LocalDateTime.now())
-            .build();
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Customer 3")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
 
-    customerMap = new HashMap<>();
-    customerMap.put(customer1.getId(), customer1);
-    customerMap.put(customer2.getId(), customer2);
-    customerMap.put(customer3.getId(), customer3);
-  }
-
-  @Override
-  public void patchCustomerById(UUID customerId, Customer customer) {
-    Customer existing = customerMap.get(customerId);
-
-    if (StringUtils.hasText(customer.getName())) {
-      existing.setName(customer.getName());
+        customerMap = new HashMap<>();
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
     }
-  }
 
-  @Override
-  public Customer saveNewCustomer(Customer customer) {
+    @Override
+    public void patchCustomerById(UUID customerId, Customer customer) {
+        Customer existing = customerMap.get(customerId);
 
-    Customer savedCustomer =
-        Customer.builder()
-            .id(UUID.randomUUID())
-            .version(1)
-            .updateDate(LocalDateTime.now())
-            .createdDate(LocalDateTime.now())
-            .name(customer.getName())
-            .build();
+        if (StringUtils.hasText(customer.getName())) {
+            existing.setName(customer.getName());
+        }
+    }
 
-    customerMap.put(savedCustomer.getId(), savedCustomer);
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        customerMap.remove(customerId);
+    }
 
-    return savedCustomer;
-  }
+    @Override
+    public void updateCustomerById(UUID customerId, Customer customer) {
+        Customer existing = customerMap.get(customerId);
+        existing.setName(customer.getName());
+    }
 
-  @Override
-  public void updateCustomerById(UUID customerId, Customer customer) {
-    Customer existingCustomer = this.customerMap.get(customerId);
-    existingCustomer.setName(customer.getName());
-    existingCustomer.setVersion(customer.getVersion());
-    existingCustomer.setCreatedDate(customer.getCreatedDate());
-    existingCustomer.setUpdateDate(LocalDateTime.now());
-    this.customerMap.put(customerId, existingCustomer);
-  }
+    @Override
+    public Customer saveNewCustomer(Customer customer) {
 
-  @Override
-  public void deleteCustomerById(UUID customerId) {
-      this.customerMap.remove(customerId);
-  }
+        Customer savedCustomer = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .updateDate(LocalDateTime.now())
+                .createdDate(LocalDateTime.now())
+                .name(customer.getName())
+                .build();
 
-  @Override
-  public Customer getCustomerById(UUID uuid) {
-    return customerMap.get(uuid);
-  }
+        customerMap.put(savedCustomer.getId(), savedCustomer);
 
-  @Override
-  public List<Customer> getAllCustomers() {
-    return new ArrayList<>(customerMap.values());
-  }
+        return savedCustomer;
+    }
+
+    @Override
+    public Customer getCustomerById(UUID uuid) {
+        return customerMap.get(uuid);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
